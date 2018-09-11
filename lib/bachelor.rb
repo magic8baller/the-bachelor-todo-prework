ed4f89fbaa181d8163732858e19dea07f8896f9a
Leslie Turis <lturis@gmail.com>
2018-09-11 22:38:16 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
+#require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash| # {'name'=>'Tessa H'..,}
+    if contestant_hash['status'] == 'Winner'
+      return contestant_hash['name'].split.first
+      #split name into 2 strings, return first name
+    end
+  end
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant_array| # 'season ', [{..},{..},..]
+    contestant_array.each do |contestant_hash| #{'name'=>'Ashley..',}
+      if contestant_hash['occupation'] == occupation
+        return contestant_hash['name']
+      end
+    end
+  end
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestant_array|
+    contestant_array.each do |contestant_hash|
+      if contestant_hash['hometown'] == hometown #if hometown matches parameter
+        count += 1 #count it
+      end
+    end
+  end
+  count
 end
+
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_array|
+    contestant_array.each do |contestant_hash|
+      if contestant_hash['hometown'] == hometown
+        return contestant_hash['occupation']
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_count = 0
+  total_ages = 0
+  data[season].each do |contestant_hash|
+    total_ages += contestant_hash['age'].to_f #float number => perform average w/ decimals
+    contestant_count += 1
+
+  end
+  (total_ages/contestant_count).round #from decimal can accurately round average
 end
